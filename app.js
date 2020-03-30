d3b8167163c3a89c43470937049d4f95f4f606cb
clarenceOnline <clarenceonline@outlook.com>
2020-03-30 18:05:37 +0000

    formated app.js file

--- app.js
+++ app.js
@@ -20,7 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-
 const express = require("express");
 
 const redisDB = require("./utils/redisDB");
